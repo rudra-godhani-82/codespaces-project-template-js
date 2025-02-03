fac6543aa02981f5cc139cc0ad89b4e855afd0e2
rudra-godhani-82 <godhanirudra@gmail.com>
2025-02-03 13:23:53 +0000

    All changes done

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,51 +1,27 @@
-/**
- * About component
- *
- * Space for you to describe more about yourself.
- */
-
 import React from "react";
 
-/**
- * About background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
- * represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a image you
- * freely use on your site.
- */
-import image from "../images/motion-background.jpg";
+import image from "../images/about-background.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "laptop with tea cup and plant";
 
-/**
- * Sort description that expands on your title on the Home component.
- */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Web Developer student studying at P.P SAVANI University,dedicated to buidling dynamic web application.I enjoy creating unique and simplistic user-friendly website in creative ways.";
 
-/**
- * List of some of skills or technologies you work on, are learning,
- * passionate about, or enjoy,
- */
 const skillsList = [
-  "Web design",
+  "Web development",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Git & GitHub",
+  "JAVA",
+  "C",
+  "Upwork Freelancer",
 ];
 
-/**
- * Use this to give more information about what you are passionate about,
- * how you best work, or even a quote. This will help someone learn more
- * about you on a professional level.
- */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my Web experience I continually look for new and better ways to make tech accessible by all. I am always looking for new opportunities to learn and grow. I am a quick learner and a team player who is always ready to take on new challenges. ";
 
 const About = () => {
   return (
@@ -53,7 +29,7 @@ const About = () => {
       <img className="background" src={image} alt={imageAltText} />
       <div
         style={{
-          backgroundColor: "white",
+          backgroundColor: "lightgray",
           width: "50%",
           padding: "4rem",
           margin: "3rem auto",
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,59 +1,28 @@
-/**
- * Portfolio component
- *
- * Highlights some of  your creations. These can be designs, websites,
- * open source contributions, articles you've written and more.
- *
- * This is a great area for you to to continually add to and refine
- * as you continue to learn and create.
- */
-
 import React from "react";
 
-/**
- * Desk image
- *
- * Below is a sample desk image. Feel free to update this to an image of your choice,
- * updating below imageAltText to string that represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
-import image from "../images/design-desk.jpeg";
+import image from "../images/mac.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "laptop with chair";
 
-/**
- * Project list
- *
- * An array of objects that will be used to display for your project
- * links section. Below is a sample, update to reflect links you'd like to highlight.
- */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    title:"Tip-Tap Editor",
+    description:"a simple text editor with basic functionalities like bold, italic, underline, and strikethrough.all the functionalities are implemented using logic.",
+    url:"https://tiptap-project.netlify.app/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Ecommerce Website",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A simple e-commerce website with basic functionalities like add to cart, remove from cart, and clear cart. The website is built using JS and styled using  CSS.",
+    url: "https://github.com/rudra-godhani-82/E_com-Web.git",
   },
   {
-    title: "My Resume Site",
+    title: "My Resume",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      " Created TDS Resume pdf file.Includes my experience and design abilities. ",
+    url: "https://drive.google.com/file/d/1DFRVa_g_DZuK-4_uXWvVyz2Ubg-JkDXc/view?usp=sharing",
   },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+
 ];
 
 const Portfolio = () => {
